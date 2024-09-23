8648efc4ba5b0e7601fb0494129dbe1695301f2e
BrunoSerpa <serpabruno2005@gmail.com>
2024-09-23 09:38:20 -0300

    47 - Criei a interface de listar sensores

--- src/types/Sensor.ts
+++ src/types/Sensor.ts
@@ -2,3 +2,9 @@ export interface ICadastrarSensor {
     nome: string,
     id_parametro: number
 };
+
+export interface IListarSensor {
+    id: number,
+    nome: string,
+    id_parametro: number,
+};
