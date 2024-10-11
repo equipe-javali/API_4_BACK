10a9a1d87d22c758f20202497427cb22f7d670c9
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-11 09:39:12 -0300

    84 - Criei a interface de listar alerta

--- src/types/Alerta.ts
+++ src/types/Alerta.ts
@@ -5,3 +5,12 @@ export interface ICadastrarAlerta {
     nome: string,
     valor: number
 };
+
+export interface IListarAlerta {
+    id: number,
+    id_estacao: number,
+    id_parametro: number,
+    condicao: string,
+    nome: string,
+    valor: number
+};
