49d8229541557c2d3dd6331f91e7d5c180729870
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-11 10:18:00 -0300

    86 - Criei a interface de atualizar alerta

--- src/types/Alerta.ts
+++ src/types/Alerta.ts
@@ -14,3 +14,12 @@ export interface IListarAlerta {
     nome: string,
     valor: number
 };
+
+export interface IAtualizarAlerta {
+    id: number,
+    id_estacao: number,
+    id_parametro: number,
+    condicao: string,
+    nome: string,
+    valor: number
+};
