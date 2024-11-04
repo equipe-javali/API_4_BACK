2f3f1d31b08c865c4919805eff9f842132561924
BrunoSerpa <serpabruno2005@gmail.com>
2024-11-04 10:48:12 -0300

    RF-5 Criei as interfaces do relatório geral TG-107

---
+++ src/types/Relatorios.ts
@@ -0,0 +1,28 @@
+export interface IBarras {
+    y: number,
+    x: string
+}
+
+export interface iFiltroRelatorios {
+    dataInicio?: Date,
+    dataFim?: Date,
+    estacoes?: number[]
+}
+
+export interface IGraficos {
+    dados: string[][],
+    titulo: string,
+    subtitulos: string[]
+}
+
+export interface IPontoMapa {
+    latitude: number,
+    longitude: number
+}
+
+export interface IRelatorios {
+    mapaEstacoes: IGraficos,
+    alertaPorEstacoes: IGraficos,
+    medicaoPorSensor: IGraficos,
+    ocorrenciaPorAlerta: IGraficos
+};
